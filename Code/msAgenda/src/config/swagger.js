17eda492020195e516538c71312aa99caf97e53b
Pedro R. Spidalieri <pedro.spidalieri@gmail.com>
2025-08-29 12:52:07 -0300

    Finished documentation for msAgenda/horarios routes

--- Code/msAgenda/src/config/swagger.js
+++ Code/msAgenda/src/config/swagger.js
@@ -25,8 +25,69 @@ swaggerSpec.components.responses = swaggerSpec.components.responses || {};
 swaggerSpec.components.parameters = swaggerSpec.components.parameters || {};
 
 // PROPIEDADES Y ESQUEMAS
+const propiedadesHorario = {
+  vendedor: {
+    type: 'string',
+    description: 'Nombre de un vendedor registrado',
+    example: 'Pedro'
+  },
+  sucursal: {
+    type: 'string',
+    description: 'Dirección de la sucursal donde trabaja el vendedor',
+    example: 'Rivadavia 967'
+  },
+
+  horaInicio: {
+    type: 'number',
+    description: 'Reloj de 24 horas',
+    example: 12
+  },
+  horaFinal: {
+    type: 'number',
+    description: 'Reloj de 24 horas',
+    example: 17
+  }
+};
+
+swaggerSpec.components.schemas.nuevoHorario = {
+  type: 'object',
+  required: ['vendedor', 'sucursal', 'diaSemana', 'horaInicio', 'horaFinal'],
+  properties: {
+      diaSemana: {
+        type: 'string',
+        example: 'Lunes',
+        enum: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+      },
+    ...propiedadesHorario
+  }
+};
+
+swaggerSpec.components.schemas.nuevosHorarios = {
+  type: 'object',
+  required: ['vendedor', 'sucursal', 'diasSemanas', 'horaInicio', 'horaFinal'],
+  properties: {
+      diasSemanas: {
+        type: ['string'],
+        example: ['Lunes', 'Martes'],
+        enum: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+        description: 'Listado de días de la semana.'
+      },
+    ...propiedadesHorario
+  }
+};
 
 // PARAMETROS
+// id Horario
+swaggerSpec.components.parameters.HorarioIdParam = {
+  name: 'id',
+  in: 'path',
+  required: true,
+  description: 'ID del horario',
+  schema: {
+    type: 'string',
+    example: '68978a6e530cf7c9ef53ebd6'
+  }
+};
 
 // RESPUESTAS
 swaggerSpec.components.responses = {
@@ -41,6 +102,16 @@ swaggerSpec.components.responses = {
   404: {
     description: 'No encontrado',
     contents: 'application/json'
+  }
+};
+
+// JWT Bearer security Schema
+swaggerSpec.components.securitySchemes = {
+  bearerAuth: {
+    type: 'http',
+    in: 'header',
+    scheme: 'bearer',
+    bearerFormat: 'JWT',
   }
 };
 
--- Code/msAgenda/src/routes/horario.route.js
+++ Code/msAgenda/src/routes/horario.route.js
@@ -1,3 +1,9 @@
+/**
+ * @swagger
+ * tags:
+ *  name: Horarios
+ *  description: Endpoints de gestión de horarios
+ */
 const express = require('express');
 const router = new express.Router();
 const { addHorario, 
@@ -7,20 +13,83 @@ const { authMiddleware, roleMiddleware } = require('../middleware/securityHandle
 const { detectarSuperposicionSimple, detectarSuperposicionMultiple } = require('../middleware/horario.middleware');
 
 // ADMIN OR SALES
+/**
+ * @swagger
+ * /api/v1/horarios/nuevo-horario:
+ *   post:
+ *     summary: Registrar horario
+ *     description: Registra un nuevo horario en la base de datos
+ *     tags: [Horarios]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/nuevoHorario'
+ *     responses:
+ *       "201":
+ *         description: Horario registrado correctamente.
+ *       "400":
+ *         $ref: '#/components/responses/400'
+ *       "401":
+ *         $ref: '#/components/responses/401'         
+ */
 router.post('/nuevo-horario', 
     authMiddleware, 
     roleMiddleware(['admin', 'sales']), 
     detectarSuperposicionSimple, 
     addHorario
 );
-
+/**
+ * @swagger
+ * /api/v1/horarios/nuevos-horarios:
+ *   post:
+ *     summary: Registra varios horarios
+ *     description: Registra varios horarios en distintos días de la semana
+ *     tags: [Horarios]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/nuevosHorarios'
+ *     responses:
+ *       "201":
+ *         description: Horarios registrados correctamente.
+ *       "400":
+ *         $ref: '#/components/responses/400'
+ *       "401":
+ *         $ref: '#/components/responses/401'         
+ */
 router.post('/nuevos-horarios', 
     authMiddleware, 
     roleMiddleware(['admin', 'sales']),
     detectarSuperposicionMultiple,
     addHorarios
 );
-
+/**
+ * @swagger
+ * /api/v1/horarios/{idHorario}:
+ *   delete:
+ *     summary: Eliminar horario
+ *     description: Elimina un horario registrado en la base de datos
+ *     tags: [Horarios]
+ *     parameters:
+ *       - $ref: '#/components/parameters/HorarioIdParam'
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "201":
+ *         description: Horario eliminado correctamente.
+ *       "401":
+ *         $ref: '#/components/responses/401'
+ *       "404":
+ *         $ref: '#/components/responses/404'             
+ */
 router.delete('/:id', 
     authMiddleware, 
     roleMiddleware(['admin', 'sales']),
